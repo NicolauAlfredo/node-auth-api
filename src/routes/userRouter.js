8ba0e75940eabd76233baf1ccc16dda68d68a4e8
NicolauAlfredo <b.nicolaualfredo@gmail.com>
2026-05-20 23:16:28 +0200

    docs(swagger): document user endpoints

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,6 +6,25 @@ const { getUserProfileController } = require("../controllers/userController");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/users/me:
+ *   get:
+ *     summary: Get authenticated user profile
+ *     tags:
+ *       - Users
+ *
+ *     security:
+ *       - bearerAuth: []
+ *
+ *     responses:
+ *       200:
+ *         description: Authenticated user retrieved successfully
+ *
+ *       401:
+ *         description: Unauthorized
+ */
+
 // Get authenticated user profile
 router.get("/me", authMiddleware, getUserProfileController);
 
